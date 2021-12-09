ce148f3f4853f52b26a9db66a14bd8b6d910fe1c
Mark <mranda10@my.centennialcollege.ca>
2021-12-08 22:56:11 -0500

    Adds some beginning logic to the form

--- MainView/Form1.cs
+++ MainView/Form1.cs
@@ -12,17 +12,68 @@ namespace MainView
 {
     public partial class Form1 : Form
     {
+
+        private List<int> inputNumbers = new List<int>();
         public Form1()
         {
             InitializeComponent();
             SetUpDefaults();
+            SetUpEvents();
         }
 
         private void SetUpDefaults()
         {
             welcomeLabel.Text = "Enter a series of numbers separated each by a space \nand then click the button when your all done";
             buttonSuccessLabel.Text = "";
-            buttonSuccessLabel.Visible = false;
+            leftErrorLabel.Text = "";
+            rightErrorLabel.Text = "";
+        }
+        private void SetUpEvents()
+        {
+            allDoneButton.Click += AllDoneButton_Click;
+        }
+
+        private void AllDoneButton_Click(object sender, EventArgs e)
+        {
+            inputNumbers.Clear();
+            string inputString = inputTxt.Text;
+
+            string[] array = inputString.Split(' ');
+            foreach (string s in array)
+            {
+                string sTrim = s.Trim();
+                try
+                {
+                    if (int.TryParse(sTrim, out int i))
+                    {
+                        inputNumbers.Add(i);
+                    }
+                    else
+                    {
+                        ArgumentException ae = new ArgumentException("Something you entered is not a number.  Check the output on the right before proceeding");
+                        inputTxt.Text = "";
+                        throw ae;
+                    }
+                }
+                catch(ArgumentException ae)
+                {
+                    leftErrorLabel.Text = ae.Message;
+                }
+                finally
+                {
+                    inputTxt.Text = "";
+                }                
+            }
+
+            DisplayInLeftBox(inputNumbers);
+        }
+
+        public void DisplayInLeftBox(List<int> numbers)
+        {
+            foreach(int num in numbers)
+            {
+                rightListBox.Items.Add(num);
+            }
         }
     }
 }
