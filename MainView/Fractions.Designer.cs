781d4acce293d1048207a7a510505acf19f8241f
Mark <mranda10@my.centennialcollege.ca>
2021-12-22 10:20:12 -0500

    Adds Form for Fractions, just placement of controls for now

--- MainView/Fractions.Designer.cs
+++ MainView/Fractions.Designer.cs
@@ -29,19 +29,153 @@ namespace MainView
         /// </summary>
         private void InitializeComponent()
         {
+            this.firstNumTxt = new System.Windows.Forms.TextBox();
+            this.secondDenTxt = new System.Windows.Forms.TextBox();
+            this.firstDenTxt = new System.Windows.Forms.TextBox();
+            this.secondNumTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resultNumTxt = new System.Windows.Forms.TextBox();
+            this.resultDenTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fractionLabel = new System.Windows.Forms.Label();
+            this.operatorComboBox = new System.Windows.Forms.ComboBox();
+            this.equalsButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // firstNumTxt
+            // 
+            this.firstNumTxt.Location = new System.Drawing.Point(107, 168);
+            this.firstNumTxt.Name = "firstNumTxt";
+            this.firstNumTxt.Size = new System.Drawing.Size(96, 27);
+            this.firstNumTxt.TabIndex = 0;
+            // 
+            // secondDenTxt
+            // 
+            this.secondDenTxt.Location = new System.Drawing.Point(278, 221);
+            this.secondDenTxt.Name = "secondDenTxt";
+            this.secondDenTxt.Size = new System.Drawing.Size(93, 27);
+            this.secondDenTxt.TabIndex = 1;
+            // 
+            // firstDenTxt
+            // 
+            this.firstDenTxt.Location = new System.Drawing.Point(107, 221);
+            this.firstDenTxt.Name = "firstDenTxt";
+            this.firstDenTxt.Size = new System.Drawing.Size(96, 27);
+            this.firstDenTxt.TabIndex = 2;
+            // 
+            // secondNumTxt
+            // 
+            this.secondNumTxt.Location = new System.Drawing.Point(278, 168);
+            this.secondNumTxt.Name = "secondNumTxt";
+            this.secondNumTxt.Size = new System.Drawing.Size(93, 27);
+            this.secondNumTxt.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(278, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "--------------";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "--------------";
+            // 
+            // resultNumTxt
+            // 
+            this.resultNumTxt.Location = new System.Drawing.Point(448, 168);
+            this.resultNumTxt.Name = "resultNumTxt";
+            this.resultNumTxt.Size = new System.Drawing.Size(93, 27);
+            this.resultNumTxt.TabIndex = 7;
+            // 
+            // resultDenTxt
+            // 
+            this.resultDenTxt.Location = new System.Drawing.Point(448, 221);
+            this.resultDenTxt.Name = "resultDenTxt";
+            this.resultDenTxt.Size = new System.Drawing.Size(93, 27);
+            this.resultDenTxt.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(448, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "--------------";
+            // 
+            // fractionLabel
+            // 
+            this.fractionLabel.AutoSize = true;
+            this.fractionLabel.Location = new System.Drawing.Point(252, 40);
+            this.fractionLabel.Name = "fractionLabel";
+            this.fractionLabel.Size = new System.Drawing.Size(154, 20);
+            this.fractionLabel.TabIndex = 10;
+            this.fractionLabel.Text = "Welcome To Fractions";
+            // 
+            // operatorComboBox
+            // 
+            this.operatorComboBox.FormattingEnabled = true;
+            this.operatorComboBox.Location = new System.Drawing.Point(218, 195);
+            this.operatorComboBox.Name = "operatorComboBox";
+            this.operatorComboBox.Size = new System.Drawing.Size(45, 28);
+            this.operatorComboBox.TabIndex = 11;
+            // 
+            // equalsButton
+            // 
+            this.equalsButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.equalsButton.Location = new System.Drawing.Point(387, 191);
+            this.equalsButton.Name = "equalsButton";
+            this.equalsButton.Size = new System.Drawing.Size(46, 29);
+            this.equalsButton.TabIndex = 12;
+            this.equalsButton.Text = "=";
+            this.equalsButton.UseVisualStyleBackColor = true;
             // 
             // Fractions
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(698, 450);
+            this.Controls.Add(this.equalsButton);
+            this.Controls.Add(this.operatorComboBox);
+            this.Controls.Add(this.fractionLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.resultDenTxt);
+            this.Controls.Add(this.resultNumTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.secondNumTxt);
+            this.Controls.Add(this.firstDenTxt);
+            this.Controls.Add(this.secondDenTxt);
+            this.Controls.Add(this.firstNumTxt);
             this.Name = "Fractions";
             this.Text = "Fractions";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox firstNumTxt;
+        private System.Windows.Forms.TextBox secondDenTxt;
+        private System.Windows.Forms.TextBox firstDenTxt;
+        private System.Windows.Forms.TextBox secondNumTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox resultNumTxt;
+        private System.Windows.Forms.TextBox resultDenTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label fractionLabel;
+        private System.Windows.Forms.ComboBox operatorComboBox;
+        private System.Windows.Forms.Button equalsButton;
     }
 }
--- MainView/Fractions.cs
+++ MainView/Fractions.cs
@@ -14,5 +14,6 @@ namespace MainView
         {
             InitializeComponent();
         }
+
     }
 }
