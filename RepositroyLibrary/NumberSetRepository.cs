2d2fe8d50ca90235a18a430a2821e00dbc03953e
Mark <mranda10@my.centennialcollege.ca>
2022-01-04 20:24:20 -0500

    Adds repository, for now data will be persisted through JSON seriliazation

---
+++ RepositroyLibrary/NumberSetRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ModelLibrary;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace RepositroyLibrary
+{
+    public class NumberSetRepository
+    {
+        private List<NumberSet> _numberSets;
+
+        public List<NumberSet> NumberSets
+        {
+            get { return _numberSets; }
+        }
+
+        public NumberSetRepository() { }
+
+        public NumberSetRepository(string path) 
+        {
+            if (File.Exists(path)) { Load(path); }
+        }
+
+        public void Load(string path)
+        {
+            string jsonString = File.ReadAllText(path);
+            _numberSets = JsonSerializer.Deserialize<List<NumberSet>>(jsonString);
+        }
+
+        public void Save(string path)
+        {
+            string jsonString = JsonSerializer.Serialize(NumberSets);
+            File.WriteAllText(path, jsonString);
+        }
+    }
+}
